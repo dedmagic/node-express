86809a81fe4486e85ed2cb00cc6c4ebeb66e1c0a
dedmagic <dedmagic@gmail.com>
2023-06-05 21:16:52 +0500

    first request

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const msg: string = "Hello world +++";
-console.log(msg);
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.end('Hello world1');
+});
+
+app.listen(port, () => {
+  console.info(`Server run on port ${port}`);
+});

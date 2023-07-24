5dba701ab849533d5f28eb283ea63de47ba5133d
dedmagic <dedmagic@gmail.com>
2023-07-24 07:39:09 +0500

    add db-utils and launch.json

---
+++ src/repositories/db-utils.js
@@ -0,0 +1,21 @@
+const sqlite = require('sqlite3').verbose();
+const { dbName } = require('../config');
+
+function openDatabase() {
+  return new sqlite.Database(dbName, sqlite.OPEN_READONLY, (err) =>
+    openDatabaseErrorHandler(err)
+  );
+}
+
+function openDatabaseErrorHandler(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+const db = openDatabase();
+
+module.exports = {
+  db,
+};
--- src/repositories/position.repository.js
+++ src/repositories/position.repository.js
@@ -1,35 +1,4 @@
-const sqlite = require('sqlite3').verbose();
-const { dbName } = require('../config');
-
-// const db = new sqlite.Database('../../database/node-express.db');
-
-// function getAllPositions() {
-//   const db = new sqlite.Database(dbName, (err) => {
-//     console.error(err);
-//   });
-//   const sql = 'SELECT * FROM positions';
-//   const allPositions = [];
-
-//   // db.serialize(() => {
-//   //   db.each(sql, (err, row) => {
-//   //     console.debug(row);
-//   //     allPositions.push(row);
-//   //   });
-//   // });
-//   // db.close();
-//   db.all(sql, (err, rows) => {
-//     console.debug({ rows });
-//   });
-//   db.close();
-//   return allPositions;
-// }
-
-const db = new sqlite.Database(dbName, (error) => {
-  if (error) {
-    console.error(error);
-    process.exit(10);
-  }
-});
+const { db } = require('./db-utils');
 
 async function getAllPositions() {
   return new Promise((resolve, reject) => {
